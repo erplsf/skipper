ceceecd0f27e062e3f4b94006f3f493262f66752
Andriy Mykhaylyk <erp.lsf@gmail.com>
2021-02-09 15:49:42 +0100

    make build work

---
+++ include_asio.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_INCLUDE_PATH "${ASIO_SOURCE_DIR}/asio-asio-1-12-1-src/asio/include")
+include_directories("${ASIO_SOURCE_DIR}/asio-asio-1-12-1-src/asio/include")
--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+#include <krpc/services/krpc.hpp>
 #include <iostream>
+#include <krpc.hpp>
 
-int main() { std::cout << "Hello World!"; }
+int main() {
+  try {
+    auto conn = krpc::connect();
+    krpc::services::KRPC krpc(&conn);
+    std::cout << "Connected to kRPC server version: "
+              << krpc.get_status().version() << std::endl;
+  } catch (...) {
+    std::cout << "Couldn't connect to the server" << std::endl;
+  };
+}
