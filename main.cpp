fb3e45ab69963f131e3a006386f2f73b1e794a4d
Andriy Mykhaylyk <erp.lsf@gmail.com>
2021-02-10 18:34:42 +0100

    actually something usable

--- main.cpp
+++ main.cpp
@@ -1,14 +1,29 @@
-#include <krpc/services/krpc.hpp>
 #include <iostream>
 #include <krpc.hpp>
+#include <krpc/services/krpc.hpp>
+#include <signal.h>
 
 int main() {
+  krpc::Client conn;
+
   try {
-    auto conn = krpc::connect();
-    krpc::services::KRPC krpc(&conn);
-    std::cout << "Connected to kRPC server version: "
-              << krpc.get_status().version() << std::endl;
-  } catch (...) {
-    std::cout << "Couldn't connect to the server" << std::endl;
-  };
+    conn = krpc::connect("skipper");
+  } catch (const std::system_error &e) {
+    std::cout << "Couldn't connect to the server, got the following error "
+                 "code: "
+              << std::endl
+              << '\t' << e.what() << std::endl
+              << "Exiting." << std ::endl;
+    exit(1);
+  }
+
+  krpc::services::KRPC krpc(&conn);
+  std::cout << "Connected to kRPC server version: "
+            << krpc.get_status().version() << std::endl;
+
+  std::cout << "Going into sleep / Waiting for any signal..." << std::endl;
+
+  sigset_t mask;
+  sigemptyset(&mask);
+  sigsuspend(&mask);
 }
