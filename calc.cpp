4c84dfbc967d0c923c1d62a7e7312e48d5056785
Andriy Mykhaylyk <erp.lsf@gmail.com>
2021-02-19 17:41:13 +0100

    It works now!

--- calc.cpp
+++ calc.cpp
@@ -1,18 +1,59 @@
-#include "calc.hpp"
-#include <cmath>
-#include <units/math.h>
+#pragma once
+
 #include <units/physical/si/derived/speed.h>
+#include <units/physical/si/si.h>
+
+#include <units/concepts.h>
+#include <units/math.h> // NOLINT(modernize-deprecated-headers)
 
 using namespace units;
+using namespace units::physical;
 
-HohmannTransferParameters::HohmannTransferParameters(GravitationalParameter auto mu_1, GravitationalParameter auto mu_2,
-                                                     Length auto r_1, Length auto r_2) :
-        mu_1(mu_1),
-        mu_2(mu_2),
-        r_1(r_1),
-        r_2(r_2) {}
+// coherent unit must apply to the system rules (in this case SI)
+struct gravitational_parameter_unit : unit<gravitational_parameter_unit> {
+};
 
-Speed auto HohmannTransferParameters::dV_apo() const {
-    return units::sqrt(mu_1 / r_1) *
-           (units::sqrt((2 * r_2 / r_1) / (1 + r_2 / r_1)) - 1);
-}
+// new derived dimensions
+struct dim_gravitational_parameter
+        : derived_dimension<dim_gravitational_parameter, gravitational_parameter_unit,
+                exponent<si::dim_length, 3>, exponent<si::dim_time, -2>> {
+};
+
+// a quantity of our dimension
+template<UnitOf <dim_gravitational_parameter> U, QuantityValue Rep = double>
+using gravitational_parameter = quantity<dim_gravitational_parameter, U, Rep>;
+
+// a concept matching the above quantity
+template<typename T>
+concept GravitationalParameter = QuantityOf<T, dim_gravitational_parameter>;
+
+class HohmannTransferParameters {
+public:
+    gravitational_parameter<gravitational_parameter_unit> mu_1;
+    gravitational_parameter<gravitational_parameter_unit> mu_2;
+    si::length<si::metre> r_1;
+    si::length<si::metre> r_2;
+
+    // TODO: Add invariants and constructors
+
+    HohmannTransferParameters(GravitationalParameter auto mu_1, GravitationalParameter auto mu_2, Length auto r_1,
+                              Length auto r_2) :
+            mu_1(mu_1),
+            mu_2(mu_2),
+            r_1(r_1),
+            r_2(r_2) {};
+
+    Speed auto dVPer() const {
+        return sqrt(mu_1 / r_1) *
+               (sqrt((2 * r_2 / r_1) / (1 + r_2 / r_1)) - 1);
+    }
+
+    Speed auto dVApo() const {
+        return sqrt(mu_1 / r_2) *
+               (1 - sqrt(2 / (1 + r_2 / r_1)));
+    }
+
+    Speed auto dVTotal() const {
+        return dVPer() + dVApo();
+    }
+};
--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
-#include "calc.hpp"
+#include "calc.cpp"
+
 #include <cmath>
 #include <iostream>
 #include <krpc.hpp>
 #include <krpc/services/krpc.hpp>
 #include <krpc/services/space_center.hpp>
-
 #include <units/physical/si/derived/speed.h>
-
-using namespace units::physical::si;
+#include <units/format.h>
+#include <units/quantity_io.h>
 
 int main() {
+    using namespace units;
+    using namespace si::literals;
+    using namespace units::physical::si::unit_constants;
+
     krpc::Client conn;
 
     try {
@@ -29,12 +33,15 @@ int main() {
 
     krpc::services::SpaceCenter sc(&conn);
 
-    auto sun = sc.bodies().at("Sun");
+    auto vessel = sc.active_vessel();
+    Length auto periapsis = si::length<si::metre>(vessel.orbit().periapsis());
     auto kerbin = sc.bodies().at("Kerbin");
-    GravitationalParameter auto gp = gravitational_parameter<gravitational_parameter_unit>(sun.gravitational_parameter());
+    GravitationalParameter auto gp = gravitational_parameter<gravitational_parameter_unit>(kerbin.gravitational_parameter());
     auto htp = HohmannTransferParameters(gp,
                                          gp,
-                                         1000_q_km, 2000_q_km);
-    const Speed auto dV = htp.dV_apo();
-    std::cout << dV << std::endl;
+                                         periapsis, periapsis + 100_q_km);
+    std::cout << fmt::format("members: {}, {}, {}, {}\n", htp.mu_1, htp.mu_2, htp.r_1, htp.r_2);
+    std::cout << fmt::format("dV required at Per: {}\n", htp.dVPer());
+    std::cout << fmt::format("dV required at Apo: {}\n", htp.dVApo());
+    std::cout << fmt::format("dV required total : {}\n", htp.dVTotal());
 }
