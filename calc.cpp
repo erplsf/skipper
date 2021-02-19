53970cad4524ffe359111ff82d1ed778429c4793
Andriy Mykhaylyk <erp.lsf@gmail.com>
2021-02-19 22:41:32 +0100

    Some refactors and a debug flag

--- calc.cpp
+++ calc.cpp
@@ -1,6 +1,5 @@
 #include <units/physical/si/derived/speed.h>
 #include <units/physical/si/si.h>
-
 #include <units/concepts.h>
 #include <units/math.h> // NOLINT(modernize-deprecated-headers)
 
@@ -8,12 +7,12 @@ using namespace units;
 using namespace units::physical;
 
 // coherent unit must apply to the system rules (in this case SI)
-struct gravitational_parameter_unit : unit<gravitational_parameter_unit> {
+struct metre_cub_per_second_sq : unit<metre_cub_per_second_sq> {
 };
 
 // new derived dimensions
 struct dim_gravitational_parameter
-        : derived_dimension<dim_gravitational_parameter, gravitational_parameter_unit,
+        : derived_dimension<dim_gravitational_parameter, metre_cub_per_second_sq,
                 exponent<si::dim_length, 3>, exponent<si::dim_time, -2>> {
 };
 
@@ -25,33 +24,34 @@ using gravitational_parameter = quantity<dim_gravitational_parameter, U, Rep>;
 template<typename T>
 concept GravitationalParameter = QuantityOf<T, dim_gravitational_parameter>;
 
-class HohmannTransferParameters {
-public:
-    gravitational_parameter<gravitational_parameter_unit> mu_1;
-    gravitational_parameter<gravitational_parameter_unit> mu_2;
-    si::length<si::metre> r_1;
-    si::length<si::metre> r_2;
-
-    // TODO: Add invariants and constructors
-
-    HohmannTransferParameters(GravitationalParameter auto mu_1, GravitationalParameter auto mu_2, Length auto r_1,
-                              Length auto r_2) :
-            mu_1(mu_1),
-            mu_2(mu_2),
-            r_1(r_1),
-            r_2(r_2) {};
+class HohmannTransfer {
+    const gravitational_parameter<metre_cub_per_second_sq> mu_1;
+    const gravitational_parameter<metre_cub_per_second_sq> mu_2;
 
     Speed auto dVPer() const {
-        return sqrt(mu_1 / r_1) *
-               (sqrt((2 * r_2 / r_1) / (1 + r_2 / r_1)) - 1);
+        return units::sqrt(mu_1 / r_1) *
+               (units::sqrt((2 * r_2 / r_1) / (1 + r_2 / r_1)) - 1);
     }
 
     Speed auto dVApo() const {
-        return sqrt(mu_1 / r_2) *
-               (1 - sqrt(2 / (1 + r_2 / r_1)));
+        return units::sqrt(mu_1 / r_2) *
+               (1 - units::sqrt(2 / (1 + r_2 / r_1)));
     }
 
-    Speed auto dVTotal() const {
-        return dVPer() + dVApo();
-    }
+public:
+    const si::length<si::metre> r_1;
+    const si::length<si::metre> r_2;
+    const si::speed<si::metre_per_second> dV_A;
+    const si::speed<si::metre_per_second> dV_P;
+    const si::speed<si::metre_per_second> dV_T;
+
+    HohmannTransfer(GravitationalParameter auto mu_1, GravitationalParameter auto mu_2, Length auto r_1,
+                    Length auto r_2) :
+            mu_1(mu_1),
+            mu_2(mu_2),
+            r_1(r_1),
+            r_2(r_2),
+            dV_A(dVApo()),
+            dV_P(dVPer()),
+            dV_T(dV_A + dV_P) {};
 };
--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@
 int main() {
     using namespace units;
     using namespace si::literals;
-    using namespace units::physical::si::unit_constants;
 
     krpc::Client conn;
 
@@ -26,21 +25,23 @@ int main() {
         exit(1);
     }
 
-    krpc::services::KRPC krpc(&conn);
-    std::cout << "Connected to kRPC server version: "
-              << krpc.get_status().version() << std::endl;
+//    krpc::services::KRPC krpc(&conn);
+//    std::cout << "Connected to kRPC server version: "
+//              << krpc.get_status().version() << std::endl;
 
     krpc::services::SpaceCenter sc(&conn);
 
     auto vessel = sc.active_vessel();
     Length auto periapsis = si::length<si::metre>(vessel.orbit().periapsis());
-    auto kerbin = sc.bodies().at("Kerbin");
-    GravitationalParameter auto gp = gravitational_parameter<gravitational_parameter_unit>(kerbin.gravitational_parameter());
-    auto htp = HohmannTransferParameters(gp,
-                                         gp,
-                                         periapsis, periapsis + 150_q_km);
-    std::cout << fmt::format("members: {}, {}, {}, {}\n", htp.mu_1, htp.mu_2, htp.r_1, htp.r_2);
-    std::cout << fmt::format("dV required at Per: {}\n", htp.dVPer());
-    std::cout << fmt::format("dV required at Apo: {}\n", htp.dVApo());
-    std::cout << fmt::format("dV required total : {}\n", htp.dVTotal());
+    auto celestialBody = vessel.orbit().body();
+    GravitationalParameter auto gp = gravitational_parameter<metre_cub_per_second_sq>(celestialBody.gravitational_parameter());
+    auto ht = HohmannTransfer(gp,
+                              gp,
+                              periapsis, periapsis + 200_q_km);
+    std::cout << fmt::format("Orbiting: ", celestialBody.name());
+    std::cout << fmt::format("r_1: {}\n", ht.r_1);
+    std::cout << fmt::format("r_2: {}\n", ht.r_2);
+    std::cout << fmt::format("dV required at Per: {}\n", ht.dV_P);
+    std::cout << fmt::format("dV required at Apo: {}\n", ht.dV_A);
+    std::cout << fmt::format("dV required total : {}\n", ht.dV_T);
 }
